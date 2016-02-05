380ba2d97d64877e5eb99e7ccee931cf8c711312
Martin Haefner <martinhaefner@web.de>
2016-02-05 17:11:56 +0100

    missing file

---
+++ src/serverside.cpp
@@ -0,0 +1,33 @@
+#include "simppl/detail/serialization.h"
+#include "simppl/interface.h"
+
+#define SIMPPL_SERVERSIDE_CPP
+#include "simppl/serverside.h"
+#undef SIMPPL_SERVERSIDE_CPP
+ 
+ 
+namespace simppl
+{
+ 
+namespace ipc
+{
+ 
+ 
+ServerRequestBase::ServerRequestBase(const char* name, detail::BasicInterface* iface)
+ : hasResponse_(false)
+ , name_(name)
+{
+   dynamic_cast<InterfaceBase<ServerRequest>*>(iface)->methods_[name_] = this;
+}
+ 
+ 
+ServerAttributeBase::ServerAttributeBase(const char* name, detail::BasicInterface* iface)
+ : name_(name)
+{
+   dynamic_cast<InterfaceBase<ServerRequest>*>(iface)->attributes_[name_] = this;
+}
+ 
+ 
+}   // namespace ipc
+
+}   // namespace simppl
