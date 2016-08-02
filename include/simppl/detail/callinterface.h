f10b17cfe4147161316af7f64cba7997d8c546b9
Martin Haefner <martinhaefner@web.de>
2016-08-02 21:04:07 +0200

    missing file

---
+++ include/simppl/detail/callinterface.h
@@ -0,0 +1,181 @@
+#ifndef SIMPPL_DETAIL_CALLINTERFACE_H
+#define SIMPPL_DETAIL_CALLINTERFACE_H
+
+
+#include "simppl/noninstantiable.h"
+#include "simppl/typelist.h"
+#include "simppl/callstate.h"
+
+
+namespace simppl
+{
+
+namespace dbus
+{
+
+namespace detail
+{
+
+// FIXME use calltraits or rvalue references here
+
+template<int N, typename TupleT>
+struct FunctionCaller
+{
+   template<typename FunctorT>
+   static inline
+   void eval(FunctorT& f, const TupleT& tuple)
+   {
+      FunctionCaller<N, TupleT>::template eval_intern(f, tuple);
+   }
+
+   template<typename FunctorT>
+   static inline
+   void eval_cs(FunctorT& f, const CallState& cs, const TupleT& tuple)
+   {
+      FunctionCaller<N, TupleT>::template eval_intern_cs(f, cs, tuple);
+   }
+
+   template<typename FunctorT, typename... T>
+   static inline
+   void eval_intern(FunctorT& f, const TupleT& tuple, const T&... t)
+   {
+     FunctionCaller<N+1 == std::tuple_size<TupleT>::value ? -1 : N+1, TupleT>::template eval_intern(f, tuple, t..., std::get<N>(tuple));
+   }
+
+   template<typename FunctorT, typename... T>
+   static inline
+   void eval_intern_cs(FunctorT& f, const CallState& cs, const TupleT& tuple, const T&... t)
+   {
+      FunctionCaller<N+1 == std::tuple_size<TupleT>::value ? -1 : N+1, TupleT>::template eval_intern_cs(f, cs, tuple, t..., std::get<N>(tuple));
+   }
+};
+
+template<typename TupleT>
+struct FunctionCaller<-1, TupleT>
+{
+   template<typename FunctorT, typename... T>
+   static inline
+   void eval_intern(FunctorT& f, const TupleT& /*tuple*/, const T&... t)
+   {
+      f(t...);
+   }
+
+   template<typename FunctorT, typename... T>
+   static inline
+   void eval_intern_cs(FunctorT& f, const CallState& cs, const TupleT& /*tuple*/, const T&... t)
+   {
+      f(cs, t...);
+   }
+};
+
+template<>
+struct FunctionCaller<0, std::tuple<>>
+{
+   template<typename FunctorT, typename... T>
+   static inline
+   void eval_cs(FunctorT& f, const CallState& cs, const std::tuple<>& tuple)
+   {
+      f(cs);
+   }
+};
+
+
+template<typename T>
+struct DeserializeAndCall : simppl::NonInstantiable
+{
+   template<typename FunctorT>
+   static inline
+   void eval(Deserializer& d, FunctorT& f)
+   {
+      std::tuple<T> tuple;
+      d.read_flattened(tuple);
+
+      FunctionCaller<0, std::tuple<T>>::template eval(f, tuple);
+   }
+
+   template<typename FunctorT>
+   static inline
+   void evalResponse(Deserializer& d, FunctorT& f, const simppl::dbus::CallState& cs)
+   {
+      std::tuple<T> tuple;
+
+      if (cs)
+         d.read_flattened(tuple);
+
+      FunctionCaller<0, std::tuple<T>>::template eval_cs(f, cs, tuple);
+   }
+};
+
+
+template<typename... T>
+struct DeserializeAndCall<std::tuple<T...>> : simppl::NonInstantiable
+{
+   template<typename FunctorT>
+   static inline
+   void eval(Deserializer& d, FunctorT& f)
+   {
+      std::tuple<T...> tuple;
+      d.read_flattened(tuple);
+
+      FunctionCaller<0, std::tuple<T...>>::template eval(f, tuple);
+   }
+
+   template<typename FunctorT>
+   static inline
+   void evalResponse(Deserializer& d, FunctorT& f, const simppl::dbus::CallState& cs)
+   {
+      std::tuple<T...> tuple;
+
+      if (cs)
+         d.read_flattened(tuple);
+
+      FunctionCaller<0, std::tuple<T...>>::template eval_cs(f, cs, tuple);
+   }
+};
+
+
+struct DeserializeAndCall0 : simppl::NonInstantiable
+{
+   template<typename FunctorT>
+   static inline
+   void eval(Deserializer& /*d*/, FunctorT& f)
+   {
+      f();
+   }
+
+   template<typename FunctorT>
+   static inline
+   void evalResponse(Deserializer& /*d*/, FunctorT& f, const simppl::dbus::CallState& cs)
+   {
+      f(cs);
+   }
+};
+
+
+template<typename... T>
+struct GetCaller : simppl::NonInstantiable
+{
+   typedef DeserializeAndCall<T...> type;
+};
+
+template<>
+struct GetCaller<> : simppl::NonInstantiable
+{
+   typedef DeserializeAndCall0 type;
+};
+
+template<>
+struct GetCaller<void> : simppl::NonInstantiable
+{
+   typedef DeserializeAndCall0 type;
+};
+
+
+}   // namespace detail
+
+}   // namespace dbus
+
+}   // namespace simppl
+
+
+#endif   // SIMPPL_DETAIL_CALLINTERFACE_H
