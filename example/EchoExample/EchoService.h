3c927264da866b01963db3570b94b6f73e3448b4
Wise86 <wise86android@gmail.com>
2017-04-06 22:00:57 +0200

    add example echo application

---
+++ example/EchoExample/EchoService.h
@@ -0,0 +1,27 @@
+#ifndef SIMPPL_ECHOSERVICE_H
+#define SIMPPL_ECHOSERVICE_H
+
+#include <string>
+#include "simppl/interface.h"
+
+namespace simppl
+{
+    namespace example {
+
+        using namespace simppl::dbus;
+
+        INTERFACE(EchoService)
+        {
+            Request<in < std::string>, out <std::string>> echo;
+
+            // constructor
+            EchoService()
+                    : INIT(echo){
+            }
+        };
+
+    }
+}   // namespace
+
+
+#endif //SIMPPL_ECHOSERVICE_H
